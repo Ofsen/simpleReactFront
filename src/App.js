227ce12209e9884621cee2362ee8c0eb1e420d89
Ofsen <ofsensmailbox@gmail.com>
2021-03-30 23:15:18 +0200

    front setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [res, setRes] = useState('');
+
+	const fetchData = async () => {
+		try {
+			const rest = await axios.get('http://localhost:3000/depinfo');
+			setRes(rest.data);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	useEffect(() => {
+		fetchData();
+		return setRes('');
+	}, []);
+
+	return (
+		<div className='App'>
+			<code>data : {JSON.stringify(res)}</code>
+			<p>
+				<b>Université:</b> {res.universite}
+			</p>
+			<p>
+				<b>Faculte:</b> {res.faculte}
+			</p>
+			<p>
+				<b>Département:</b> {res.departement}
+			</p>
+		</div>
+	);
+};
 
 export default App;
